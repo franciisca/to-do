1a3a4828b929fc9c7d1a66f6127bbf1351462cb4
Fran <frannii.sv@gmail.com>
2022-08-13 15:52:19 +0900

    set middleware

--- server.js
+++ server.js
@@ -15,3 +15,88 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName)
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/',(request, response)=>{
+    const todoItems = await db.collection('things-todo').find().toArray()
+    const itemsLeft = await db.collection('things-todo').countDocuments({completed: false})
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error))
+})
+
+app.post('/addTodo', (request, response) => {
+    db.collection('things-todo').insertOne({thing: request.body.todoItem, completed: false})
+    .then(result => {
+        console.log('To-do Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/markComplete', (request, response) => {
+    db.collection('things-todo').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.put('/markUnComplete', (request, response) => {
+    db.collection('things-todo').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteItem', (request, response) => {
+    db.collection('things-todo').deleteOne({thing: request.body.itemFromJS})
+    .then(result => {
+        console.log('To-do task deleted')
+        response.json('To-do task deleted')
+    })
+    .catch(error => console.error(error))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
